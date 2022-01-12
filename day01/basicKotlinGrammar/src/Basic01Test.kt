58b49f3f3e7b53ecbc81e526ba1aaf896f0a635c
seonghyunlee <tlgj255@naver.com>
2022-01-12 23:22:45 +0900

    Add : kotlin 마지막 예제 추가

--- day01/basicKotlinGrammar/src/Basic01Test.kt
+++ day01/basicKotlinGrammar/src/Basic01Test.kt
@@ -1,12 +1,11 @@
 fun main() {
-    test1()
 }
 
 fun test1() {
-    for(i in 0..8) {
-        for(j in 0 .. 12) {
+    for (i in 0..8) {
+        for (j in 0..12) {
             print(
-                if(i + j == 6 || j - i == 6 || i == 6 || i == 2 || i - j == 2 || i + j == 14) {
+                if (i + j == 6 || j - i == 6 || i == 6 || i == 2 || i - j == 2 || i + j == 14) {
                     "*"
                 } else {
                     " "
@@ -16,3 +15,18 @@ fun test1() {
         println()
     }
 }
+
+fun test2() =
+    arrayListOf(1, 2, 3, 4, 5, 6)
+        .also {
+            println(it)
+        }.apply {
+            add(7)
+            add(8)
+        }.map {
+            it + 3
+        }.filter {
+            it > 5
+        }.run {
+            get(2)
+        }
