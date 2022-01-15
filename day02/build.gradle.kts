d140e39fa17e5267e8b6ad402609924757141b66
seonghyunlee <tlgj255@naver.com>
2022-01-16 00:14:04 +0900

    Add : lang3 dependency

--- day02/build.gradle.kts
+++ day02/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.commons:commons-lang3")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
