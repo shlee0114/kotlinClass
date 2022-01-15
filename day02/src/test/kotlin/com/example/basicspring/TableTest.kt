284bfd8c1dbf652cf9e3f1db4ec1676446b82f6f
seonghyunlee <tlgj255@naver.com>
2022-01-16 00:14:14 +0900

    Add : fail 관련 test code

--- day02/src/test/kotlin/com/example/basicspring/TableTest.kt
+++ day02/src/test/kotlin/com/example/basicspring/TableTest.kt
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -87,6 +88,19 @@ class TableTest {
     }
 
     @Test
+    fun tableUpdate_returnReason_fail() {
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test1\", \"title\" : \"test1\", \"content\" : \"test1\", \"password\" : \"test2\"}"
+        )
+            .andDo(print())
+            .andExpect(status().isCreated)
+            .andExpect(jsonPath("$.success", `is`(false)))
+            .andExpect(jsonPath("$.data.reason", `is`("패스워드가 일치하지 않습니다")))
+            .andExpect(jsonPath("$.error", `is`(IsNull.nullValue())))
+    }
+
+    @Test
     fun tableDelete_returnTrue_success() {
         doDelete("/table/1/test")
             .andDo(print())
@@ -94,6 +108,160 @@ class TableTest {
             .andExpect(jsonPath("$.success", `is`(true)))
             .andExpect(jsonPath("$.data.result", `is`(true)))
             .andExpect(jsonPath("$.error", `is`(IsNull.nullValue())))
+    }
+
+    @Test
+    fun tableDelete_returnReason_fail() {
+        doDelete("/table/1/test1")
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.success", `is`(false)))
+            .andExpect(jsonPath("$.data.reason", `is`("패스워드가 일치하지 않습니다")))
+            .andExpect(jsonPath("$.error", `is`(IsNull.nullValue())))
+    }
+
+    @Test
+    fun tableInfo_throwException_NotFound() {
+        doGet("/table/999")
+            .checkIsError(status().isNotFound, ErrorMessage.INVALID_VALUE, "999")
+    }
+
+    @Test
+    fun tableAdd_throwException_InvalidValue() {
+        doPost(
+            "/table",
+            "{\"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "writer")
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "title")
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "content")
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "password")
+    }
+
+    @Test
+    fun tableUpdate_throwException_invalidValue() {
+        doPut(
+            "/table/1",
+            "{\"title\" : \"test1\", \"content\" : \"test1\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "writer")
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test1\", \"content\" : \"test1\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "title")
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test1\", \"title\" : \"test1\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "content")
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test1\", \"title\" : \"test1\", \"content\" : \"test1\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_VALUE, "password")
+    }
+
+    @Test
+    fun tableAdd_throwException_invalidFormat() {
+        doPost(
+            "/table",
+            "{\"writer\" : \"test!@#\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_FORMAT, "writer!@#한글, 숫자, 영어")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"t\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "writer!@#2 ~ 10")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"t11132d12dd2d\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "writer!@#2 ~ 10")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"${randomAlphanumeric(101)}\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "title!@#0 ~ 100")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"${randomAlphanumeric(501)}\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "content!@#0 ~ 500")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"t\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "password!@#2 ~ 10")
+
+        doPost(
+            "/table",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"t12d12f12g1wgw\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "password!@#2 ~ 10")
+    }
+
+    @Test
+    fun tableUpdate_throwException_invalidFormat() {
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test!@#\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.INVALID_FORMAT, "writer!@#한글, 숫자, 영어")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"t\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "writer!@#2 ~ 10")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"t11132d12dd2d\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "writer!@#2 ~ 10")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test\", \"title\" : \"${randomAlphanumeric(101)}\", \"content\" : \"test\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "title!@#0 ~ 100")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"${randomAlphanumeric(501)}\", \"password\" : \"test\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "content!@#0 ~ 500")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"t\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "password!@#2 ~ 10")
+
+        doPut(
+            "/table/1",
+            "{\"writer\" : \"test\", \"title\" : \"test\", \"content\" : \"test\", \"password\" : \"t12d12f12g1wgw\"}"
+        )
+            .checkIsError(status().isBadRequest, ErrorMessage.OUT_OF_RANGE, "password!@#2 ~ 10")
     }
 
     fun ResultActions.checkIsError(status: ResultMatcher, errorCode: ErrorMessage, additionalMessage: String = "") =
