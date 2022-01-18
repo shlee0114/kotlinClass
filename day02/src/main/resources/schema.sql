57b33d6a6ca2b092c641e66787b63aa488dc809c
tlgj255 <tlgj255@naver.com>
2022-01-18 21:29:22 +0900

    Add : Default ddl

---
+++ day02/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE notice
+(
+    seq      BIGINT        NOT NULL AUTO_INCREMENT,
+    writer   VARCHAR2(10)  NOT NULL,
+    title    VARCHAR2(100) NOT NULL,
+    content  VARCHAR2(500) NOT NULL,
+    password VARCHAR2(10) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY (seq)
+);
+
+INSERT INTO notice (writer, title, content, password) VALUES ('test', 'test', 'test', 'test')
