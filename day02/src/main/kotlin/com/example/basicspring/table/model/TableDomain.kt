09443d092f44535233049e00e836f518b0736bbd
seonghyunlee <tlgj255@naver.com>
2022-01-19 23:18:56 +0900

    Change : table name

--- day02/src/main/kotlin/com/example/basicspring/table/model/TableDomain.kt
+++ day02/src/main/kotlin/com/example/basicspring/table/model/TableDomain.kt
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
-@Entity(name = "table")
+@Entity(name = "notice")
 data class TableDomain(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
