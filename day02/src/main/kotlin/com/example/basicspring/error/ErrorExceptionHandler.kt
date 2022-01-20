ad94a2ec09c73590649645e8de0b6652a8390dec
seonghyunlee <tlgj255@naver.com>
2022-01-20 23:06:14 +0900

    Style : clean code

--- day02/src/main/kotlin/com/example/basicspring/error/ErrorExceptionHandler.kt
+++ day02/src/main/kotlin/com/example/basicspring/error/ErrorExceptionHandler.kt
@@ -6,37 +6,36 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.validation.ConstraintViolationException
 
-@RestControllerAdvice
+@ControllerAdvice
 class ErrorExceptionHandler {
     private val _log = LoggerFactory.getLogger(javaClass)
 
+    private fun newResponse(throwable: Throwable, status: HttpStatus) =
+        newResponse(throwable.message, status)
+
+    private fun newResponse(message: String?, status: HttpStatus) =
+        ResponseEntity(ErrorMessage.getErrorMessage(message ?: "NONE"), status)
+            .also {
+                _log.error(message)
+            }
+
     @ExceptionHandler(CustomException::class)
     fun customErrorHandler(e : CustomException): ResponseEntity<ApiUtils> =
-        ResponseEntity(ErrorMessage.getErrorMessage(e.message ?: ""), e.status)
-            .also {
-                _log.error(e.message)
-            }
-    @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class, MethodArgumentNotValidException::class)
+        newResponse(e, e.status)
+
+    @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class, MethodArgumentNotValidException::class, ConstraintViolationException::class)
     fun badRequestHandler(e: RuntimeException): ResponseEntity<ApiUtils> =
-        ResponseEntity(ErrorMessage.getErrorMessage(e.message?:""), HttpStatus.BAD_REQUEST)
-            .also {
-                _log.error(e.message)
-            }
+        newResponse(e, HttpStatus.BAD_REQUEST)
 
     @ExceptionHandler(NoSuchElementException::class)
     fun notFoundHandler(e: RuntimeException): ResponseEntity<ApiUtils> =
-        ResponseEntity(ErrorMessage.getErrorMessage(e.message ?: ""), HttpStatus.NOT_FOUND)
-            .also {
-                _log.error(e.message)
-            }
+        newResponse(e, HttpStatus.NOT_FOUND)
 
     @ExceptionHandler(RuntimeException::class)
     fun error5xxHandler(e: RuntimeException): ResponseEntity<ApiUtils> =
-        ResponseEntity(ErrorMessage.getErrorMessage(e.message ?: ""), HttpStatus.INTERNAL_SERVER_ERROR)
-            .also {
-                _log.error(e.message)
-            }
+        newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR)
 }
