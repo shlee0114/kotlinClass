0df700f376a1ba00a2d27da4f3f1d1ddc84765e7
seonghyunlee <tlgj255@naver.com>
2022-02-02 13:09:15 +0900

    Add : response id 추가

--- day02/src/main/kotlin/com/example/basicspring/table/model/TableDto.kt
+++ day02/src/main/kotlin/com/example/basicspring/table/model/TableDto.kt
@@ -1,11 +1,13 @@
 package com.example.basicspring.table.model
 
 data class TableDto(
+    val id: Long,
     val writer: String,
     val title: String,
     val content: String = "",
 ) {
     constructor(tableDomain: TableDomain) : this(
+        id = tableDomain.seq?:0L,
         writer = tableDomain.writer,
         title = tableDomain.title,
         content = tableDomain.content
