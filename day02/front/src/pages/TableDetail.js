87b18ac66c389fcf3b1cbec817f9be78972b0387
seonghyunlee <tlgj255@naver.com>
2022-02-02 13:29:18 +0900

    Add : table detail

--- day02/front/src/pages/TableDetail.js
+++ day02/front/src/pages/TableDetail.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useParams} from "react-router-dom";
+import axios from 'axios'
+
+const TableInfo = ({writer, title, content}) => {
+    return(
+        <div>
+            writer : {writer}<br/>
+            title : {title}<br/>
+            content : {content}<br/>
+        </div>
+    )
+}
 
 export const TableDetail = () => {
+    const [table, setTableInfo] = useState([])
+    const params = useParams();
+
+    useEffect(() => {
+        axios(
+            {
+                url : `http://localhost:5000/table/${params.tableId}`,
+                method : 'GET'
+            }
+        ).then( response => {
+            console.log(response)
+            setTableInfo(response.data.data)
+        })
+    }, [])
+
     return (
         <main>
-            {useParams().tableId}
+        {<TableInfo writer={table.writer} title={table.title} content={table.content}/>}
         </main>
     )
 }
