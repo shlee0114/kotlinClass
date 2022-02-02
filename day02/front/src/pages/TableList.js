e8489786803ddaeea63f4b981f1c379379d6a1a9
seonghyunlee <tlgj255@naver.com>
2022-02-02 13:10:12 +0900

    Add : table list 표출 화면 추가

--- day02/front/src/pages/TableList.js
+++ day02/front/src/pages/TableList.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+
+const Table = ({writer, title, id}) => {
+    const navigate = useNavigate();
+  
+    return (
+        <tr onClick={() => {navigate(`/${id}`)}}>
+            <td>
+                {writer}
+            </td>
+            <td>
+                {title}
+            </td>
+        </tr>
+    )
+}
 
 export const TableList = () => {
+    const [table, setTableList] = useState([])
+    useEffect(() => {
+        axios(
+            {
+                url : 'http://localhost:5000/table',
+                method : 'GET'
+            }
+        ).then( response => {
+            console.log(response)
+            setTableList(response.data.data)
+        })
+    }, [])
     return (
-        <div>
-        </div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            작성자
+                        </td>
+                        <td>
+                            제목
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {table.map(rowData => {
+                        return (
+                            <Table writer={rowData.writer} title={rowData.title} id={rowData.id}/>
+                        )
+                    })}
+                </tbody>
+            </table>
     )
 }
